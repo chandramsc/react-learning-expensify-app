9d44fcab74aefc1ee706d7952eac4c37a607c9b9
chandrasekaran <chandrasekaran@tartlabs.com>
2018-07-05 07:20:21 +0530

    Spreading Objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -27,7 +27,17 @@ const removeExpense = ({ id } = {}) => ({
 });
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -43,7 +53,18 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
                 action.expense
             ];
         case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => id !== action.id);            
+            return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                };
+            });
         default:
             return state
     }
@@ -59,6 +80,11 @@ const filtersReducerDefaultState = {
 }
 const filtersReducer = (state = filtersReducerDefaultState,action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default: return state
     }
 }
@@ -78,7 +104,12 @@ store.subscribe(() => {
 const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 100 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'coffe', amount: 300 }));
 
+
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
@@ -97,3 +128,14 @@ const demoState = {
 };
 
 // console.log(demoState);
+
+// const user = {
+//     name : 'chandra',
+//     age: 27
+// };
+
+// console.log({
+//     age: 24,
+//     ...user,
+//     location: 'pollachi'
+// })
