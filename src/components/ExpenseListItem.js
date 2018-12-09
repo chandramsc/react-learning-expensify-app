f2d3d7a93855925f258db20a72173d53020aec1d
chandra <chandramsc4@gmail.com>
2018-12-09 16:46:38 +0530

    Rendering Individual Expenses

---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Export a stateless functional component
+// description, amount, createdAt
+
+const ExpenseListItem = ({description, amount, createdAt}) => (
+    <div>
+        <h3>{description}</h3>
+        <p>{amount} - {createdAt}</p>
+    </div>
+);
+
+export default ExpenseListItem;
