3bbe7fb4fd68a7027f0db5ef127f7ecea4cdfc17
Chandrasekaran <chandrasekaran@tartlabs.com>
2018-12-27 15:20:12 +0530

    Creating Separate CSS Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 // console.log(path.join(__dirname, 'public'));
 
 module.exports = (env) => {
-    const isProduction = env === 'production'    
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin("styles.css");
     return {
         entry: './src/app.js',
         output: {
@@ -17,14 +18,28 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader', // creates style nodes from JS strings
-                    'css-loader', // translates CSS into CommonJS
-                    'sass-loader' // compiles Sass to CSS
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
-        devtool: isProduction ? 'source-map' : 'cheap-eval-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
